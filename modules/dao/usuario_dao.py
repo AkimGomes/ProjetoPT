5158522c588e9b432fa037607560d27b771abd25
AkimGomes <ak-gomes@hotmail.com>
2022-04-02 02:15:23 -0300

    Alterando de usuario_id para id, no dao de Usuário

--- modules/dao/usuario_dao.py
+++ modules/dao/usuario_dao.py
@@ -13,18 +13,6 @@ class UsuarioDAO:
         usuario = self._db_conn.query(Usuario).filter(Usuario.email_do_usuario == usuario).first()
         return usuario
 
-    def pega_usuario_email(self, usuario_email):
-        usuario_email = self._db_conn.query(Usuario).filter(Usuario.email_do_usuario == usuario_email).first()
-        return usuario_email
-
-    def pega_usuario_cpf(self, usuario_cpf):
-        usuario_cpf = self._db_conn.query(Usuario).filter(Usuario.cpf_do_usuario == usuario_cpf).first()
-        return usuario_cpf
-
-    def pega_usuario_pis(self, usuario_pis):
-        usuario_pis = self._db_conn.query(Usuario).filter(Usuario.pis_do_usuario == usuario_pis).first()
-        return usuario_pis
-
     def registra_usuario(self, usuario):
         try:
             self._db_conn.add(usuario)
@@ -36,7 +24,7 @@ class UsuarioDAO:
 
     def altera_usuario(self, usuario_id, novas_info_usuario):
         try:
-            self._db_conn.query(Usuario).filter(Usuario.usuario_id == usuario_id).update({
+            self._db_conn.query(Usuario).filter(Usuario.id == usuario_id).update({
                 Usuario.nome_do_usuario: novas_info_usuario.nome_do_usuario,
                 Usuario.email_do_usuario: novas_info_usuario.email_do_usuario,
                 Usuario.senha_do_usuario: novas_info_usuario.senha_do_usuario,
@@ -50,5 +38,5 @@ class UsuarioDAO:
             self._db_conn.close()
 
     def deleta_usuario(self, usuario_id):
-        self._db_conn.query(Usuario).filter(Usuario.usuario_id == usuario_id).delete()
+        self._db_conn.query(Usuario).filter(Usuario.id == usuario_id).delete()
         self._db_conn.commit()
