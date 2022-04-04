932a08f8eeab3b07461ce209e55746ef7117b585
AkimGomes <ak-gomes@hotmail.com>
2022-04-04 04:54:26 -0300

    Adicionando tratamento de excessão para os campos de País e Estado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_login import LoginManager, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.exceptions import BadRequestKeyError
 
 from modules.db import Usuario, db_session
 from modules.dao.usuario_dao import UsuarioDAO
@@ -57,12 +58,38 @@ def _verifica_pis_e_senha_do_usuario(usuario, senha):
 @app.route('/registra-usuario', methods=['GET', 'POST'])
 def registra_usuario():
     if request.method == 'POST':
-        pis = usuario_dao.pega_usuario_login_pis(request.form['pis'])
-        cpf = usuario_dao.pega_usuario_login_cpf(request.form['cpf'])
-        email = usuario_dao.pega_usuario_login_email(request.form['email'])
-        if pis or cpf or email:
-            flash('Dados inválidos. CPF, PIS ou Email já cadastrados!', 'error')
-        else:
+        try:
+            pis = usuario_dao.pega_usuario_login_pis(request.form['pis'])
+            cpf = usuario_dao.pega_usuario_login_cpf(request.form['cpf'])
+            email = usuario_dao.pega_usuario_login_email(request.form['email'])
+            if pis or cpf or email:
+                flash('Dados inválidos. CPF, PIS ou Email já cadastrados!', 'error')
+            else:
+                usuario = Usuario(request.form['nome'],
+                                  request.form['email'],
+                                  generate_password_hash(request.form['senha']),
+                                  request.form['cpf'],
+                                  request.form['pis'],
+                                  request.form['pais'],
+                                  request.form['estado'],
+                                  request.form['municipio'],
+                                  request.form['cep'],
+                                  request.form['rua'],
+                                  request.form['numero'],
+                                  request.form['complemento'])
+                usuario_dao.registra_usuario(usuario)
+                flash('Usuário cadastrado com sucesso!', 'success')
+                return redirect(url_for('loga_usuario'))
+        except BadRequestKeyError:
+            flash('Por favor, preencha os dados de Pais e Estado', 'error')
+    return render_template('cadastro_de_usuario.html')
+
+
+@app.route('/informacoes-editaveis-do-usuario', methods=['GET', 'POST'])
+@login_required
+def edita_info_do_usuario():
+    if request.method == 'POST':
+        try:
             usuario = Usuario(request.form['nome'],
                               request.form['email'],
                               generate_password_hash(request.form['senha']),
@@ -75,31 +102,11 @@ def registra_usuario():
                               request.form['rua'],
                               request.form['numero'],
                               request.form['complemento'])
-            usuario_dao.registra_usuario(usuario)
-            flash('Usuário cadastrado com sucesso!', 'success')
-            return redirect(url_for('loga_usuario'))
-    return render_template('cadastro_de_usuario.html')
-
-
-@app.route('/informacoes-editaveis-do-usuario', methods=['GET', 'POST'])
-@login_required
-def edita_info_do_usuario():
-    if request.method == 'POST':
-        usuario = Usuario(request.form['nome'],
-                          request.form['email'],
-                          generate_password_hash(request.form['senha']),
-                          request.form['cpf'],
-                          request.form['pis'],
-                          request.form['pais'],
-                          request.form['estado'],
-                          request.form['municipio'],
-                          request.form['cep'],
-                          request.form['rua'],
-                          request.form['numero'],
-                          request.form['complemento'])
-        usuario_dao.altera_usuario(request.args.get('id'), usuario)
-        flash('Alterações feitas com sucesso!', 'success')
-        return redirect(url_for('mostra_menu_usuario'))
+            usuario_dao.altera_usuario(request.args.get('id'), usuario)
+            flash('Alterações feitas com sucesso!', 'success')
+            return redirect(url_for('mostra_menu_usuario'))
+        except BadRequestKeyError:
+            flash('Por favor, preencha os dados de Pais e Estado', 'error')
     return render_template('informacoes_do_usuario_logado.html')
 
 
