7918b00f7d68df7d1792b8d5c14d900e7d3a9332
AkimGomes <ak-gomes@hotmail.com>
2022-04-06 03:17:44 -0300

    Retirando as funções não utilizadas no projeto do DAO de endereço

--- modules/dao/endereco_dao.py
+++ modules/dao/endereco_dao.py
@@ -5,55 +5,9 @@ class EnderecoDAO:
     def __init__(self, db_conn):
         self._db_conn = db_conn
 
-    def pega_endereco_id(self, endereco_id):
-        endereco_id = self._db_conn.query(Endereco).filter(Endereco.endereco_id == endereco_id).first()
-        return endereco_id
-
     def pega_endereco_por_id_usuario(self, usuario_id):
         endereco = self._db_conn.query(Endereco).filter(Endereco.fk_id_usuario == usuario_id).first()
         return endereco
-
-    def pega_endereco_pais(self, endereco_id):
-        pais_endereco = self._db_conn.query(Endereco.pais_do_usuario).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return pais_endereco
-
-    def pega_endereco_estado(self, endereco_id):
-        estado_endereco = self._db_conn.query(Endereco.estado_do_usuario).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return estado_endereco
-
-    def pega_endereco_municipio(self, endereco_id):
-        municipio_endereco = self._db_conn.query(Endereco.municipio_do_usuario).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return municipio_endereco
-
-    def pega_endereco_cep(self, endereco_id):
-        cep_endereco = self._db_conn.query(Endereco.cep_do_usuario).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return cep_endereco
-
-    def pega_endereco_rua(self, endereco_id):
-        rua_endereco = self._db_conn.query(Endereco.rua_do_usuario).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return rua_endereco
-
-    def pega_endereco_numero(self, endereco_id):
-        numero_endereco = self._db_conn.query(Endereco.numero_da_rua).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return numero_endereco
-
-    def pega_endereco_complemento(self, endereco_id):
-        complemento_endereco = self._db_conn.query(Endereco.complemento).filter(Endereco.endereco_id == endereco_id).first()
-        self._db_conn.expunge_all()
-        self._db_conn.close()
-        return complemento_endereco
 
     def registra_endereco(self, endereco):
         try:
